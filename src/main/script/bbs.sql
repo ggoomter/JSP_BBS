6827ed44e641731b7d94e0875ecc75b4b042fb68
ggoomter <ggoomter@gmail.com>
2021-11-19 23:51:39 +0900

    변수써서 insert

--- src/main/script/bbs.sql
+++ src/main/script/bbs.sql
@@ -12,6 +12,7 @@ create table BBS(
 	primary key (bbsID)
 );
 
+alter table BBS add VIEWCOUNT INT default 0; 
 
 select * from BBS;
 
@@ -27,6 +28,11 @@ insert into BBS values (11, '11번글', 'ggoomter', now(), '아무내용', 1);
 insert into BBS values (12, '12번글', 'ggoomter', now(), '아무내용', 1);
 
 
-alter table BBS add VIEWCOUNT INT default 0; 
+
+set @bbsNum := (select MAX(bbsID) from BBS);
+INSERT INTO bbs.bbs
+(bbsID, bbsTitle, userID, bbsDate, bbsContent, bbsAvailable, VIEWCOUNT)
+VALUES(@bbsNum+1, concat(@bbsNum+1,'번글'), 'ggoomter', '2021-11-13 19:42:52', '아무내용', 1, 0);
+
 
 commit;
